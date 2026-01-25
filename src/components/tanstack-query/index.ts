f111dab14a062896897cf5a3aa6f6c2daa321d3d
gilberto-freitas <gilberto@crewfare.com>
2026-01-25 03:38:57 -0300

    feat: migrated from bun to pnpm with tanstack start

---
+++ src/components/tanstack-query/index.ts
@@ -0,0 +1,7 @@
+export { TanstackQueryProvider, getQueryContext } from './TanstackQueryProvider'
+export { tanstackQueryDevtoolsConfig } from './TanstackQueryDevtools'
+export type {
+	TanstackQueryProviderProps,
+	TanstackQueryContext,
+	TanstackQueryDevtoolsConfig,
+} from './types'
