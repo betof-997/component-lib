e85fd88b2e03802702aa8c7bfdd85dc048775da6
gilberto-freitas <gilberto@crewfare.com>
2026-01-24 23:22:12 -0300

    feat: add card component

---
+++ src/components/card/index.ts
@@ -0,0 +1,10 @@
+export { Card } from './Card'
+export type {
+	CardActionProps,
+	CardContentProps,
+	CardDescriptionProps,
+	CardFooterProps,
+	CardHeaderProps,
+	CardRootProps,
+	CardTitleProps,
+} from './types'
---
+++ src/components/card/types.ts
@@ -0,0 +1,15 @@
+import type { ComponentProps } from 'react'
+
+export type CardRootProps = ComponentProps<'div'>
+
+export type CardHeaderProps = ComponentProps<'div'>
+
+export type CardTitleProps = ComponentProps<'div'>
+
+export type CardDescriptionProps = ComponentProps<'div'>
+
+export type CardActionProps = ComponentProps<'div'>
+
+export type CardContentProps = ComponentProps<'div'>
+
+export type CardFooterProps = ComponentProps<'div'>
